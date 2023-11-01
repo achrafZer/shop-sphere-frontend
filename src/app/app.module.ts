58dced0db078839246ba9e9ab58ab295bdf64753
KPOTY Kpotivi <kpotivi.kpoty@etu.univ-amu.fr>
2023-11-01 13:57:50 +0100

    fix: updating app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ProductComponent } from './components/product/product.component';
-import { OrdersComponent } from './components/orders/orders.component';
-import { OrderComponent } from './components/order/order.component';
-import { BuyerComponent } from './components/buyer/buyer.component';
-import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsComponent,
-    ProductComponent,
-    OrdersComponent,
-    OrderComponent,
-    BuyerComponent,
-    AdminComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
