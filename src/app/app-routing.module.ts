ef895c0e3ab60d9160e48a25ad0b0a7f6f18e629
KPOTY Kpotivi <kpotivi.kpoty@etu.univ-amu.fr>
2023-11-03 23:56:29 +0100

    feat: adding routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { BuyerComponent } from './components/buyer/buyer.component';
+import { OrderComponent } from './components/order/order.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: ProductsComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'me', component: BuyerComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'orders/:id', component: OrderComponent },
+  { path: 'products/:id', component: ProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
