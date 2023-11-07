6e1a8dd5d77298a9da85fcf1af76c9e969e5e4c1
KPOTY Kpotivi <kpotivi.kpoty@etu.univ-amu.fr>
2023-11-07 01:48:24 +0100

    feat: added login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { BuyerComponent } from './components/buyer/buyer.component';
+import { LoginFormComponent } from './components/forms/login-form/login-form.component';
 import { OrderComponent } from './components/order/order.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { ProductComponent } from './components/product/product.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: ProductsComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'login', component: LoginFormComponent },
   { path: 'me', component: BuyerComponent },
   { path: 'orders', component: OrdersComponent },
   { path: 'orders/:id', component: OrderComponent },
--- src/app/services/guard.service.ts
+++ src/app/services/guard.service.ts
@@ -1,12 +1,16 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 /*
  * This function control the access to a path
  * by checking wether user is connected or not
+ * and redirect to home if not
  */
 export const canActivate: CanActivateFn = () => {
   const authService = inject(AuthService);
-  return authService.isAuthenticatedUser();
+  const router = inject(Router);
+  if (authService.isAuthenticatedUser()) return true;
+  router.navigate(['home']);
+  return false;
 };
